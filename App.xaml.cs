099099ad91288126dde00aacdc4fe06fadda7022
LiamMcGowanNP <LiamMcGowanNP@Outlook.com>
2021-08-18 12:55:33 +0100

    InitialProgress

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using System.Reflection;
+using System.Diagnostics;
+using Ini;
 
 namespace TrainingProject
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    /// 
+    internal delegate void Invoker();
     public partial class App : Application
     {
+        public App()
+        {
+
+            // Populate global variables
+            LoadAppVariables gen = new LoadAppVariables();
+
+            gen.LoadApplication();
+
+            //ApplicationInitialize = _applicationInitialize;
+        }
     }
 }
